2e2be769f4455f717f7669f51c2ae5ea6beb3922
durvanshudd <durvanshudd@gmail.com>
2026-08-31 11:05:48 +0530

    Delete Queries directory

--- Queries/10_Monthly_revenue_at_risk.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    COUNT(*) AS churned_high_risk_customers,
-    ROUND(SUM(MonthlyCharges), 2) AS monthly_revenue_at_risk,
-	ROUND(AVG(MonthlyCharges), 2) AS average_monthly_revenue_at_risk
-FROM telco_customer_churn
-WHERE contract = 'Month-to-month'
-  AND tenure <= 12
-  AND internetservice = 'Fiber optic'
-  AND churn = 'Yes';
